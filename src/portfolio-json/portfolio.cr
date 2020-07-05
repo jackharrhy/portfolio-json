66e2f03a6a29557d6a43fc8349ece8efe1396e88
Jack Arthur Harrhy <me@jackharrhy.com>
2020-07-05 16:44:27 -0230

    specify full module path, might fix bug with this lib imported elsewhere

--- src/portfolio-json/portfolio.cr
+++ src/portfolio-json/portfolio.cr
@@ -1,7 +1,7 @@
 class PortfolioJson::Portfolio
   include JSON::Serializable
 
-  property repos : Array(Github::Repo) = Github::Repo.repos_of_user
+  property repos : Array(PortfolioJson::Github::Repo) = PortfolioJson::Github::Repo.repos_of_user
 
   def initialize
   end
